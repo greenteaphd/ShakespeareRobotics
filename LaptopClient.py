cb178853d83dac2c80f2f53f9ae6e872fabc692f
Andy Han <dhan@macalester.edu>
2018-04-14 18:35:06 -0500

    Gave the robot an accent, pitch, and tempo of speech. Sounds more like an actor.

--- LaptopClient.py
+++ LaptopClient.py
@@ -4,17 +4,19 @@
 
 import socket
 
+hamlet_soliloquy = "To be, or not to be--that is the question: Whether 'tis nobler in the mind to suffer. "
+
 
 def client_main(server_address):
     """ This method takes in a server address in order to send data to the server. """
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((server_address, 7000))
-    data = "Hello Juliet! What a beautiful evening!"
+    data = hamlet_soliloquy
     data = data.encode()
     s.sendall(data)
     print("SUCCESS!")
     s.close()
     return
 
-client_main("169.254.26.110")
+client_main("169.254.228.234")
 
--- RobotServer.py
+++ RobotServer.py
@@ -3,11 +3,14 @@
 # April 8th, 2019
 
 import socket
+import ev3dev.ev3 as ev3
 
 
 def test_method(string_input):
     """ Tests the server_main method to see if the data was sent correctly. """
     print("The client sent: " + string_input)
+    ev3.Sound.set_volume(100)
+    ev3.Sound.speak(string_input, espeak_opts= '-a 200 -s 130 -v "en-uk-wmids" -g 7 -k 20')
 
 
 def server_main(server_address):
@@ -16,7 +19,7 @@ def server_main(server_address):
     s.bind((server_address, 7000))
     s.listen(10)
 
-    while True:
+    for i in range(1):
         print("The server is up and running!")
         conn, address = s.accept()
         data = conn.recv(1024)
@@ -24,5 +27,5 @@ def server_main(server_address):
         test_method(data)
         conn.close()
 
-server_main("192.168.0.1")
+server_main("169.254.228.234")
 
