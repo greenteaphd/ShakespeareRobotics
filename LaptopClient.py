e4a9c7969e30fafa6a986afad9c60f494c9ad3b1
JoJo Hsiao <jhsiao@macalster.edu>
2018-04-15 13:36:52 -0500

    Added my API key, the tester Text-To-Speech example, and added it into the LaptopClient file as well.

--- LaptopClient.py
+++ LaptopClient.py
@@ -4,14 +4,38 @@
 
 import socket
 
-hamlet_soliloquy = "To be, or not to be--that is the question: Whether 'tis nobler in the mind to suffer. "
+import speech_recognition as sr
+
+with open("api-key.json") as f:
+    GOOGLE_CLOUD_SPEECH_CREDENTIALS = f.read()
+
+# Record Audio
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+humanInput = ""
+# Speech recognition using Google Speech Recognition
+try:
+    humanInput = r.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS)
+    print("You said: " + r.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+if len(humanInput) > 0:
+    humanInput
+else:
+    humanInput = "Try again"
 
 
 def client_main(server_address):
     """ This method takes in a server address in order to send data to the server. """
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((server_address, 7000))
-    data = hamlet_soliloquy
+    data = humanInput
     data = data.encode()
     s.sendall(data)
     print("SUCCESS!")
